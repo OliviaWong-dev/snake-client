002bf92c47814c075bedb6850e0bf97f6a27c320
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-14 18:00:30 +0000

    modified

--- client.js
+++ client.js
@@ -13,6 +13,10 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+  });
+
   conn.on('data', (data) => {
     console.log(data.toString());
   });
