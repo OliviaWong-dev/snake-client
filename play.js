681d61613ff3e403ab6cc265f8cdfed01115ce91
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-14 20:29:58 +0000

    input module completed

--- play.js
+++ play.js
@@ -1,5 +1,9 @@
 
-const connect = require('./client')
+const connect = require('./client');
+const setupInput = require('./input');
+const setUpInput = require('./input');
+
 console.log('Connecting ...');
 connect();
 
+setupInput();
