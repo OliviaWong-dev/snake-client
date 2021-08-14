e4cb4cc856a9f32a9ece88203fa1d35f0ea38cfb
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-14 18:42:29 +0000

    experiment move

--- client.js
+++ client.js
@@ -3,12 +3,12 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
-  
+
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
@@ -19,7 +19,22 @@ const connect = function() {
 
   conn.on('connect', () => {
     conn.write("Name: OWW");
+    // conn.write("Move: up")
+
+    // SETINTERVAL
+    // setInterval(() => {
+    // }, 3000)
+
+    // SETTIMEOUT
+    // let delay = 1000;
+    // for (let i = 0; i < move.length; i++) {
+    //   setTimeout(() => {
+    //     conn.write(move[i])
+    //   }, delay);
+    //   delay += 3000;
+    // };
   });
+
 
   conn.on('data', (data) => {
     console.log(data.toString());
@@ -27,5 +42,7 @@ const connect = function() {
 
   return conn;
 };
+
+// const move = ["Move: up", "Move: down", "Move: left", "Move: right" ]
 
 module.exports = connect;
