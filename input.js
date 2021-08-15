0064feb51c5bc29ade81bdce42d0673b71bc79e3
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-15 19:09:22 +0000

    sending message

--- input.js
+++ input.js
@@ -2,22 +2,25 @@ let connection;
 
 const handleUserInput = (key) => {
   if (key === 'w') {
-    connection.write("Move: up")
+    connection.write("Move: up");
   } else if (key === 'a') {
-    connection.write("Move: left")
+    connection.write("Move: left");
   } else if (key === 's') {
-    connection.write("Move: down")
+    connection.write("Move: down");
   } else if (key === 'd') {
-    connection.write("Move: right")
+    connection.write("Move: right");
+  } else if (key === 'z') {
+    connection.write(`Say: ${message}`);
   } else if (key === '\u0003') {
     process.exit();
   }
 };
 
+const message = "bye";
   
 
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
